5f8efc5639c32fdfebd4ba680d7f5b56066965e0
Shivam060 <yadavshiv633@gmail.com>
2026-02-03 14:34:13 +0530

    New Changes

--- src/app/HomePage.js
+++ src/app/HomePage.js
@@ -1,12 +1,16 @@
-import Accreditation from "@/components/Accreditation";
-import MainSection from "@/components/MainSection";
 import React from "react";
+import MainSection from "@/components/MainSection";
+import Accreditation from "@/components/Accreditation";
+import ProgrammeOverview from "@/components/ProgrammeOverview";
+import ProgrammeHighlights from "@/components/ProgrammeHighlights";
 
 export default function HomePage() {
     return (
         <>
             <MainSection />
             <Accreditation />
+            <ProgrammeOverview />
+            <ProgrammeHighlights />
         </>
     );
 };
