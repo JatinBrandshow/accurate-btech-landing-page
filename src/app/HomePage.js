a84618580de3631f855b449baaa930a4ecef0bbd
Shivam060 <yadavshiv633@gmail.com>
2026-02-03 18:47:13 +0530

    New Changes

--- src/app/HomePage.js
+++ src/app/HomePage.js
@@ -3,6 +3,8 @@ import MainSection from "@/components/MainSection";
 import Accreditation from "@/components/Accreditation";
 import ProgrammeOverview from "@/components/ProgrammeOverview";
 import ProgrammeHighlights from "@/components/ProgrammeHighlights";
+import WhyUs from "@/components/WhyUs";
+import ProgrammeCurriculum from "@/components/ProgrammeCurriculum";
 
 export default function HomePage() {
     return (
@@ -11,6 +13,8 @@ export default function HomePage() {
             <Accreditation />
             <ProgrammeOverview />
             <ProgrammeHighlights />
+            <WhyUs />
+            <ProgrammeCurriculum />
         </>
     );
 };
